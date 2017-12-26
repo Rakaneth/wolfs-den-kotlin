4c18d3184461eba5d5867214525bc2b42cf17018
Quincy West <rakaneth@gmail.com>
2017-12-25 21:43:10 -0500

    Initial Commit

---
+++ core/src/main/kotlin/wolfsden/WolfGame.kt
@@ -0,0 +1,8 @@
+package wolfsden
+
+import com.badlogic.gdx.ApplicationAdapter
+import squidpony.squidgrid.gui.gdx.SparseLayers
+
+class WolfGame: ApplicationAdapter() {
+	private var display: SparseLayers? = null
+}
---
+++ desktop/src/main/java/wolfsden/desktop/DesktopLauncher.java
@@ -0,0 +1,28 @@
+package wolfsden.desktop;
+
+import com.badlogic.gdx.Files.FileType;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import wolfsden.WolfGame;
+
+/** Launches the desktop (LWJGL) application. */
+public class DesktopLauncher {
+    public static void main(String[] args) {
+        createApplication();
+    }
+
+    private static LwjglApplication createApplication() {
+        return new LwjglApplication(new WolfGame(), getDefaultConfiguration());
+    }
+
+    private static LwjglApplicationConfiguration getDefaultConfiguration() {
+        LwjglApplicationConfiguration configuration = new LwjglApplicationConfiguration();
+        configuration.title = "wolfs-den-kotlin";
+        configuration.width = 80 * 10;
+        configuration.height = 31 * 20;
+        for (int size : new int[] { 128, 64, 32, 16 }) {
+            configuration.addIcon("libgdx" + size + ".png", FileType.Internal);
+        }
+        return configuration;
+    }
+}
