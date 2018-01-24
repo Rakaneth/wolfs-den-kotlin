dc1f8567c8c45f5676c9d8b82734bd46456ae567
Quincy West <rakaneth@gmail.com>
2018-01-24 11:56:51 -0500

    add more effects

--- core/src/main/kotlin/wolfsden/entity/effects/Buffs.kt
+++ core/src/main/kotlin/wolfsden/entity/effects/Buffs.kt
@@ -20,3 +20,13 @@ class HasteEffect(
         override val eID: String,
         override var duration: Int
 ) : Effect("Haste", eID, duration = duration, atkDly = -5, movDly = -5, buff = true)
+
+class MightEffect(
+        override val eID: String,
+        override var duration: Int
+) : Effect("Might", eID, duration = duration, atk = 2, dmg = 2, buff = true)
+
+class AgilityEffect(
+        override val eID: String,
+        override var duration: Int
+) : Effect("Agility", eID, duration = duration, dfp = 2, sav = 2, buff = true)
--- core/src/main/kotlin/wolfsden/entity/effects/Debuffs.kt
+++ core/src/main/kotlin/wolfsden/entity/effects/Debuffs.kt
@@ -4,3 +4,28 @@ class StunEffect(
         override val eID: String,
         override var duration: Int
 ) : Effect("Stunned", eID, duration = duration, loseTurn = true)
+
+class WolfCurseEffect(
+        override val eID: String,
+        override var duration: Int
+) : Effect("Wolf Curse", eID, duration = duration, weakness = listOf("wolf", "natural"))
+
+class DarkCurseEffect(
+        override val eID: String,
+        override var duration: Int
+) : Effect("Curse of Darkness", eID, duration = duration, weakness = listOf("dark", "magic"))
+
+class SlowEffect(
+        override val eID: String,
+        override var duration: Int
+) : Effect("Slowed", eID, duration = duration, movDly = 5, atkDly = 5)
+
+class WeakEffect(
+        override val eID: String,
+        override var duration: Int
+) : Effect("Weakened", eID, duration = duration, atk = -2, dmg = -2)
+
+class BreachEffect(
+        override val eID: String,
+        override var duration: Int
+) : Effect("Breached", eID, duration = duration, dfp = -2, sav = -2)
